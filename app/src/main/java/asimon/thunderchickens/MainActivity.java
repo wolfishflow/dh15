1f16e8d8ac218265d2af8319fce1034a39cd9be6
alan simon <alancarlsimon@gmail.com>
2017-03-05 00:32:14 -0500

    volley singleton for request created

--- app/src/main/java/asimon/thunderchickens/MainActivity.java
+++ app/src/main/java/asimon/thunderchickens/MainActivity.java
@@ -1,8 +1,17 @@
 package asimon.thunderchickens;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by alansimon on 2017-03-04.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
---
+++ app/src/main/java/asimon/thunderchickens/data/VolleySingleton.java
@@ -0,0 +1,52 @@
+package asimon.thunderchickens.data;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by alansimon on 2017-03-05.
+ */
+
+public class VolleySingleton {
+    private static VolleySingleton mVolleySingletonInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mContext;
+
+    private VolleySingleton(Context context){
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+
+    }
+
+    public static synchronized VolleySingleton getInstance(Context context) {
+        if (mVolleySingletonInstance == null) {
+            mVolleySingletonInstance = new VolleySingleton(context);
+        }
+        return mVolleySingletonInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(tag);
+        getRequestQueue().add(req);
+    }
+
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
