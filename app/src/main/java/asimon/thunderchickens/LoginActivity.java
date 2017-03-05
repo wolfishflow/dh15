fe2b1560e2781b25c253071b7e9d1783b304a556
alan simon <alancarlsimon@gmail.com>
2017-03-05 01:05:46 -0500

    button click now works???? @click and onclick didn't work

--- app/src/main/java/asimon/thunderchickens/LoginActivity.java
+++ app/src/main/java/asimon/thunderchickens/LoginActivity.java
@@ -1,13 +1,39 @@
 package asimon.thunderchickens;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.logging.Logger;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Play some music...
+                Toast.makeText(LoginActivity.this, "heloo", Toast.LENGTH_SHORT).show();
+                Log.d("Help", "help");
+            }
+        });
+
     }
+
 }
